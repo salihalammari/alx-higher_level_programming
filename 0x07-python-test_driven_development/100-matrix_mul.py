d334d8c6a51225093c2d330892294f45d358f729
salihalammari <salihalammari91@gmail.com>
2023-09-06 03:56:30 -0700

    d

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-matrix_mul = __import__('100-matrix_mul').matrix_mul
-
-print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
-print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
-
-
-++++++++++++++++++++++++++++++++
-
-100-matrix_mul.py
-
-#!/usr/bin/python3
 # 100-matrix_mul.py
 """Defines a matrix multiplication function."""
 

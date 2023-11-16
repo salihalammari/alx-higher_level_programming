5bfbc3b0fb17612398c95293246ea83eed700c1e
salihalammari <slammari@student.1337.ma>
2023-11-16 14:47:54 -0800

    don

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,10 @@
-7-occurrences.js
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let nOccurrences = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      nOccurrences++;
+    }
+  }
+  return nOccurrences;
+};
